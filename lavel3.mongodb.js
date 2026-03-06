d62074f367ca41a960bcf57332b383ddc05ffc6d
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-07 02:31:09 +0530

    lavel3 complete

---
+++ lavel3.mongodb.js
@@ -0,0 +1,39 @@
+// show('dbs')
+use("ecommerce");
+
+// db.createCollection('products')
+
+// db.products.insertMany([
+//   {
+//     name: "iPhone 13",
+//     price: 999,
+//     category: "Electronics",
+//     stock: 50,
+//   },
+//   {
+//     name: "Samsung Galaxy S21",
+//     price: 899,
+//     category: "Electronics",
+//     stock: 30,
+//   },
+//   {
+//     name: "Sony PlayStation 5",
+//     price: 499,
+//     category: "Gaming",
+//     stock: 20,
+//   },
+//   {
+//     name: "Dell XPS 13",
+//     price: 1199,
+//     category: "Computers",
+//     stock: 15,
+//   },
+// ]);
+
+
+// db.products.find()
+
+// db.products.find({category:"Electronics"})
+
+// db.products.find().count()
+

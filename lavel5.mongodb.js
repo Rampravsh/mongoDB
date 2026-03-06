5f6d718baa0118d1552f1103f7748a48e6f6f2f1
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-07 03:06:06 +0530

    lavel 5 complete

---
+++ lavel5.mongodb.js
@@ -0,0 +1,11 @@
+// use('shopApp')
+// db.users.find()
+// db.users.deleteOne({name:"lakhan kumar"})
+
+// db.users.updateMany({name:"amit kumar"},{$set:{age:17}})
+
+// db.users.deleteMany({age:{$lt:18}})
+
+// use('ecommerce')
+// db.products.deleteOne({})
+// db.products.find()

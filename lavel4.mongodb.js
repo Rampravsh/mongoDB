303d45c3d278a532e5226d4d9d4fbd36a4a962b1
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-07 02:53:31 +0530

    lavel 4 complete

---
+++ lavel4.mongodb.js
@@ -0,0 +1,30 @@
+// show("dbs");
+// use('ecommerce')
+// show('collections')
+
+// db.products.find()
+
+// use("shopApp");
+
+// db.users.updateMany({city:'delhi'},{$set:{city:'Mumbai'}})
+
+// db.users.find()
+
+// use("ecommerce");
+
+// db.products.updateOne({},{$set:{price:10000}})
+
+// db.products.updateMany(
+//   {},
+//   {
+//     $set: { stock: 20 },
+//   },
+// );
+// db.products.find()
+
+// db.products.updateMany({ category: "Electronics" }, { $inc: { stock: 20 } });
+
+// use("shopApp");
+// show('collections')
+// db.users.updateOne({email:'amit@example.com'}, {$set:{email:'amitkr@gmail.com'}});
+// db.users.find()

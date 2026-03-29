7978089f21b98f63a74293be08017a1f78cc70bb
Rampravesh <rampraveshkr4545@gmail.com>
2026-03-29 23:14:38 +0530

    pagination

--- cursor/cursor.mongodb.js
+++ cursor/cursor.mongodb.js
@@ -1,11 +1,11 @@
-show ('dbs')
-use ('test')
+show("dbs");
+use("test");
 // show ('collections')
 
 // let arr=[]
 
 // for (let index = 0; index < 100; index++) {
-//     arr.push({val:index})    
+//     arr.push({val:index})
 // }
 
 // db.data.insertMany(arr)
@@ -19,13 +19,16 @@ use ('test')
 // console.log(cursor.next())
 // console.log(cursor.hasNext())
 
-
 // while(cursor.hasNext()){
 //     console.log(cursor.next())
 // }
 
-
 // db.data.find().sort({val:-1})
 
-db.data.find().limit(5)
+// db.data.find().limit(5)
 
+// db.data.find().skip(5).limit(10)
+
+let page = 2;
+const limit = 10;
+db.data.find().skip((page-1)*limit).limit(limit);
